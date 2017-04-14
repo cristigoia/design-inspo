ee433ac8fd74c88f473383b03de80a3b5202a32b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-04-14 20:50:21 +0200

    Debugging .env reading issue.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'isomorphic-fetch';
 import Feed from '../components/Feed';
+import fs from 'fs';
+require('dotenv').config();
 
-const postEndpoint = `http://twnsndco.dropmark.com/396720.json`;
+const postEndpoint = `https://mihailgaberov.dropmark.com/414652.json?key=${process.env.DROPMARK_KEY}`;
 
 export default class Index extends React.Component {
+
   static async getInitialProps() {
+    console.log('>>> ', postEndpoint);
     const response = await fetch(postEndpoint);
     const json = await response.json();
     return {
